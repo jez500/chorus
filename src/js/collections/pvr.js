0a06c9b67e5baca5021099a378ccec5d3910da18
Jeremy Graham <mail@jez500.com>
2014-04-18 22:51:46 +0800

    removed debugging

--- src/js/collections/pvr.js
+++ src/js/collections/pvr.js
@@ -7,48 +7,10 @@ app.PvrChannelCollection = Backbone.Collection.extend({
 
   sync: function(method, model, options) {
 
-    var items = [{
-      "thumbnail": '',
-      "channeltype": 'tv',
-      "hidden": null,
-      "locked": null,
-      "channel": 'TEN',
-      "lastplayed": null,
-      "channelid": 10
-    },{
-      "thumbnail": '',
-      "channeltype": 'tv',
-      "hidden": null,
-      "locked": null,
-      "channel": 'TEN11',
-      "lastplayed": null,
-      "channelid": 11
-    },{
-      "thumbnail": '',
-      "channeltype": 'tv',
-      "hidden": null,
-      "locked": null,
-      "channel": 'TEN',
-      "lastplayed": null,
-      "channelid": 12
-    },{
-      "thumbnail": '',
-      "channeltype": 'tv',
-      "hidden": null,
-      "locked": null,
-      "channel": 'TEN22',
-      "lastplayed": null,
-      "channelid": 13
-    }];
-
-    options.success(items);
-
-    return;
-
-//    // remove above
-//    app.pvr.getChannels(options.type, function(items){
-//      options.success(items);
-//    });
+    // remove above
+    app.pvr.getChannels(options.type, function(items){
+      options.success(items);
+    });
 
   }
 
